008fb5db3813c6a315e6c33abadc67b5526e9bf4
PavanPadamata <its4tech54@gmail.com>
2022-10-14 20:22:04 +0530

    BringUp For EternityOS Official

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -11,15 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Evolution-X Stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit common EternityOS Stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
 
-# Evolution-X Stuff
-EVO_BUILD_TYPE := OFFICIAL
+
+# EternityOS Stuff
+CUSTOM_BUILD_TYPE := OFFICIAL
+ETERNITY_MAINTAINER := ραναи-ρα∂αмαтα
+
+# GAPPS Flag
+WITH_GAPPS := true
 
 PRODUCT_NAME := evolution_sweet
 PRODUCT_DEVICE := sweet
